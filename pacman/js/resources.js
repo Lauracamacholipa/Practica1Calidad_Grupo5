2d9a74cbe70ef852aabb6f72c5b46f82c4dacbb6
Jorge Vargas <jorge.vargas.p@ucb.edu.bo>
2025-09-24 14:29:05 -0400

    Agrega cobertura de tests para resources.js

--- pacman/js/resources.js
+++ pacman/js/resources.js
@@ -1,37 +1,26 @@
-
+// pacman/js/resources.js
 (function() {
     let resourceCache = {};
-    let loading = [];
     let readyCallbacks = [];
 
-    // Load an image url or an array of image urls
     function load(urlOrArr) {
-        if(urlOrArr instanceof Array) {
-            urlOrArr.forEach(function(url) {
-                _load(url);
-            });
-        }
-        else {
+        if (urlOrArr instanceof Array) {
+            urlOrArr.forEach(url => _load(url));
+        } else {
             _load(urlOrArr);
         }
     }
 
     function _load(url) {
-        if(resourceCache[url]) {
-            return resourceCache[url];
-        }
-        else {
-            let img = new Image();
-            img.onload = function() {
-                resourceCache[url] = img;
-                
-                if(isReady()) {
-                    readyCallbacks.forEach(function(func) { func(); });
-                }
-            };
-            resourceCache[url] = false;
-            img.src = url;
-        }
+        if (resourceCache[url]) return resourceCache[url];
+
+        const img = new Image();
+        img.onload = function() {
+            resourceCache[url] = img;
+            if (isReady()) readyCallbacks.forEach(f => f());
+        };
+        resourceCache[url] = false;
+        img.src = url;
     }
 
     function get(url) {
@@ -39,24 +28,15 @@
     }
 
     function isReady() {
-        let ready = true;
-        for(let k in resourceCache) {
-            if(resourceCache.hasOwnProperty(k) &&
-               !resourceCache[k]) {
-                ready = false;
-            }
-        }
-        return ready;
+        return Object.values(resourceCache).every(r => r);
     }
 
     function onReady(func) {
         readyCallbacks.push(func);
     }
 
-    window.resources = { 
-        load: load,
-        get: get,
-        onReady: onReady,
-        isReady: isReady
-    };
+    const resObj = { load, get, isReady, onReady };
+
+    if (typeof window !== 'undefined') window.resources = resObj;
+    if (typeof module !== 'undefined' && module.exports) module.exports = resObj;
 })();
